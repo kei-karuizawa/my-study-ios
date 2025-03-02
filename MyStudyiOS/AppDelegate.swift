5dbf6c596f892657180ba75f5c545262827dfeb0
月见坂桐叶 <git_kiriha@163.com>
2025-03-02 22:47:38 +0800

    feature：增加计算器 Demo

--- MyStudyiOS/AppDelegate.swift
+++ MyStudyiOS/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let viewController = ViewController()
+        let viewController = CalculatorViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         navigationController.navigationBar.backgroundColor = .red
         navigationController.navigationBar.isTranslucent = false
